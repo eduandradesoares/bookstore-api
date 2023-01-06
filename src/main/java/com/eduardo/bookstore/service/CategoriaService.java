856f7d228784eb291f015eae1ecb083af6612bc2
eduandradesoares <eduandradesoares@gmail.com>
2023-01-06 14:07:22 -0300

    Get para livro por ID

--- src/main/java/com/eduardo/bookstore/service/CategoriaService.java
+++ src/main/java/com/eduardo/bookstore/service/CategoriaService.java
@@ -21,7 +21,7 @@ public class CategoriaService {
 	public Categoria findById(Integer id) {
 		Optional<Categoria> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
-				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+				"Categoria não encontrada! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 
 	public List<Categoria> findAll() {
---
+++ src/main/java/com/eduardo/bookstore/service/LivroService.java
@@ -0,0 +1,26 @@
+package com.eduardo.bookstore.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.eduardo.bookstore.domain.Livro;
+import com.eduardo.bookstore.repositories.LivroRepository;
+import com.eduardo.bookstore.service.exceptions.ObjectNotFoundException;
+
+@Service
+public class LivroService {
+	
+	@Autowired
+	private LivroRepository repository;
+	
+	public Livro findById(Integer id) {
+		Optional<Livro> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Livro não encontrado! Id: " + id + ", Tipo: " + Livro.class.getName()));
+		
+	}
+	
+	
+
+}
