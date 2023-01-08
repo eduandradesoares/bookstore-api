cbf98a9d5d74e631ba639f5abdf1ba21620e9c57
eduandradesoares <eduandradesoares@gmail.com>
2023-01-07 21:54:18 -0300

    POST para livro

--- src/main/java/com/eduardo/bookstore/service/LivroService.java
+++ src/main/java/com/eduardo/bookstore/service/LivroService.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.eduardo.bookstore.domain.Categoria;
 import com.eduardo.bookstore.domain.Livro;
 import com.eduardo.bookstore.repositories.LivroRepository;
 import com.eduardo.bookstore.service.exceptions.ObjectNotFoundException;
@@ -40,6 +41,13 @@ public class LivroService {
 		newObj.setNome_autor(obj.getNome_autor());
 		newObj.setTexto(obj.getTexto());
 		
+	}
+
+	public Livro create(Integer id_cat, Livro obj) {
+		obj.setId(null);
+		Categoria cat = categoriaService.findById(id_cat);
+		obj.setCategoria(cat);
+		return repository.save(obj);
 	}
 	
 	
