d38f013565538ffdebe79942b47a5cffe6146e5e
eduandradesoares <eduandradesoares@gmail.com>
2023-01-07 22:00:02 -0300

    DELETE para livro

--- src/main/java/com/eduardo/bookstore/service/LivroService.java
+++ src/main/java/com/eduardo/bookstore/service/LivroService.java
@@ -49,6 +49,12 @@ public class LivroService {
 		obj.setCategoria(cat);
 		return repository.save(obj);
 	}
+
+	public void delete(Integer id) {
+		Livro obj = findById(id);
+		repository.delete(obj);
+		
+	}
 	
 	
 
